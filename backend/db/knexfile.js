586b302d7db6d4d0bf81d7132f31cf462312eee7
Pol Texido <107403548+calss0t@users.noreply.github.com>
2022-11-08 20:00:23 +0900

    Created backend folder and frontend folder. Installed basic dependencies. Created knex file in backend.

---
+++ backend/db/knexfile.js
@@ -0,0 +1,34 @@
+require('dotenv').config({path:__dirname+'/../../.env.local'})
+// require('dotenv').config({path: "./.env.local"})
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./migrations"
+    },
+    seeds: {
+        directory: "./seeds"
+      }
+  },
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+        directory: "./migrations"
+    },
+    seeds: {
+        directory: "./seed"
+    }
+  }
+
+};
