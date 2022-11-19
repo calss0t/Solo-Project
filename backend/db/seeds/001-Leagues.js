57c0af58dff1e4ff21bb2358951656c463458fa3
Pol Texido <107403548+calss0t@users.noreply.github.com>
2022-11-19 13:54:10 +0900

    Added more sports options

--- backend/db/seeds/001-Leagues.js
+++ backend/db/seeds/001-Leagues.js
@@ -16,5 +16,12 @@ exports.seed = async function(knex) {
     {id: 8, league_Id: '3', League_Id_TheSportsDB: '4481', league_name:"UEFA Europa League", league_logo:"https://media.api-sports.io/football/leagues/3.png"},
     {id: 9, league_Id: '135', League_Id_TheSportsDB: '4332', league_name:"Serie A", league_logo:"https://media.api-sports.io/football/leagues/135.png"},
     {id: 10, league_Id: '253', League_Id_TheSportsDB: '4346', league_name:"Major League Soccer", league_logo:"https://media.api-sports.io/football/leagues/253.png"},
+    {id: 11, league_Id: '12', League_Id_TheSportsDB: '4387', league_name:"NBA", league_logo:"https://media.api-sports.io/basketball/leagues/12.png"},
+    {id: 12, league_Id: '99999', League_Id_TheSportsDB: '4370', league_name:"Formula 1", league_logo:"https://www.thesportsdb.com/images/media/league/logo/jiqa741556460666.png/preview"},
+    {id: 13, league_Id: '99998', League_Id_TheSportsDB: '4443', league_name:"UFC", league_logo:"https://www.thesportsdb.com/images/media/league/logo/r52yzu1613970430.png/preview"},
+    {id: 14, league_Id: '117', League_Id_TheSportsDB: '4408', league_name:"LIGA ACB", league_logo:"https://media.api-sports.io/basketball/leagues/117.png"},
+    {id: 15, league_Id: '99996', League_Id_TheSportsDB: '4424', league_name:"Major League Baseball", league_logo:"https://www.thesportsdb.com/images/media/league/badge/c5r83j1521893739.png/preview"},
+    {id: 16, league_Id: '99997', League_Id_TheSportsDB: '4391', league_name:"NFL", league_logo:"https://www.thesportsdb.com/images/media/league/badge/g85fqz1662057187.png/preview"},
+
   ]);
 };
